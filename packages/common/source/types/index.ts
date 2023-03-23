c25e2f94955c3a225bfa023ebc1a3c679118b0cb
Jess Tucker <jessjesstheappreciator@gmail.com>
2023-03-22 21:57:30 -0400

    Update common types

--- packages/common/source/types/index.ts
+++ packages/common/source/types/index.ts
@@ -5,13 +5,13 @@ export enum SOCKET_EVENTS {
 
 export enum GAME_EVENTS {
     START_GAME = "start_game",
-    ON_START_GAME = "on_start_game",
+    // ON_START_GAME = "on_start_game",
     READY_FOR_GAME = "ready_for_game",
-    ON_READY_FOR_GAME = "on_ready_for_game",
-    UPDATE_GAME = "update_game",
-    ON_UPDATE_GAME = "on_update_game",
+    // ON_READY_FOR_GAME = "on_ready_for_game",
+    UPDATE_GAME = "update_game", // the outgoing event from client->server
+    ON_UPDATE_GAME = "on_update_game", // the incoming event from server->client.  
     WIN_GAME = "win_game",
-    ON_WIN_GAME = "on_win_win",
+    ON_WIN_GAME = "on_win_game",
 }
 
 export const GAME_CONSTANTS = {
@@ -19,12 +19,12 @@ export const GAME_CONSTANTS = {
 }
 
 export enum ROOM_EVENTS {
-    JOIN_GAME = "join_game",
-    ON_JOIN_GAME = "on_join_game",
-    JOIN_GAME_ERROR = "join_game_error",
-    ROOM_JOINED = "room_joined",
+    JOIN_ROOM = "join_room",
+    // ON_JOIN_GAME = "on_join_game",
+    JOIN_ROOM_ERROR = "join_room_error",
+    JOIN_ROOM_SUCCESS = "join_room_success",
     ROOM_READY = "room_ready",
-    JESS_TEST = "SOMETHING"
+    ROOM_DISCONNECT = "room_disconnect"
 }
 
 export const ROOM_CONSTANTS = {
